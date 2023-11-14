f15e21285fca8eaed0ab32cde30e725f672164c7
Marvin Smith <marvin_smith1@me.com>
2023-11-13 22:12:55 -0700

    Adding latest block of non-functional code.

--- include/terminus/math/linalg/Operations.hpp
+++ include/terminus/math/linalg/Operations.hpp
@@ -6,23 +6,11 @@
 #pragma once
 
 // Terminus Libraries
+#include <terminus/math/linalg/SVD.hpp>
 #include <terminus/math/matrix.hpp>
 #include <terminus/math/vector.hpp>
 
 namespace tmns::math::linalg {
-
-namespace detail {
-template <typename MatrixT,
-          typename VectorT>
-typename VectorT::value_type calc_threshold( const Matrix_Base<MatrixT>& A,
-                                             const Vector_Base<VectorT>& S )
-{
-      return typename VectorT::value_type( 0.5
-                * sqrt(double(A.impl().cols()) + double(A.impl().rows()) + 1.)
-                * S.impl()[0]
-                * std::numeric_limits<typename VectorT::value_type>::epsilon() );
-}
-} // End of detail namespace
 
 /**
  * Solve for nullity .. using previous SVD results
@@ -32,47 +20,54 @@ template <typename MatrixT,
           typename VectorT>
 size_t nullity( const Matrix_Base<MatrixT>&  A,
                 const Matrix_Base<MatrixT2>& U,
-                const Vector_Base<VectorT>& S,
-                const MatrixBase<MatrixT2>& V,
+                const Vector_Base<VectorT>&  S,
+                const Matrix_Base<MatrixT2>& V,
                 typename VectorT::value_type thresh = -1 )
 {
     using value_type = typename MatrixT::value_type;
 
     value_type th = ( thresh >= 0. ? thresh : detail::calc_threshold(A, S) );
-    size_t nn = boost::numeric_cast<size_t>(A.impl().cols()-S.impl().size());
+    size_t nn = boost::numeric_cast<size_t>( A.impl().cols() - S.impl().size() );
     for ( size_t j = 0; j < S.impl().size(); j++ ) {
       if ( S.impl()[j] <= th )
         nn++;
     }
     return nn;
-  }
+}
 
-  // Nullity of a matrix (again from Numerical Receipes)
-  template <class MatrixT>
-  inline size_t nullity( MatrixBase<MatrixT> const& A,
-                         typename MatrixT::value_type thresh = -1 ) {
+/**
+ * Nullity of a matrix (again from Numerical Receipes)
+ */
+template <typename MatrixT>
+size_t nullity( const Matrix_Base<MatrixT>& A,
+                typename MatrixT::value_type thresh = -1 )
+{
+    using value_type = typename MatrixT::value_type;
+    MatrixN<value_type> U, V;
+    VectorN<value_type> S;
+    complete_svd( A.impl(), U, S, V );
+    V = transpose( V );
+    return nullity( A, U, S, V, thresh );
+}
+
+/**
+ * Solve for the nullspace of a Matrix A. If Ax = [0], the nullspace is an x that is not zero.
+ */
+template <typename MatrixT>
+MatrixN<typename MatrixT::value_type> nullspace( const Matrix_Base<MatrixT>&  A,
+                                                 typename MatrixT::value_type thresh = -1 )
+{
     typedef typename MatrixT::value_type value_type;
-    Matrix<value_type> U, V;
-    Vector<value_type> S;
+    MatrixN<value_type> U, V;
+    VectorN<value_type> S;
     complete_svd( A.impl(), U, S, V );
     V = transpose(V);
-    return nullity(A,U,S,V,thresh);
-  }
 
-  // Solve for the nullspace of a Matrix A. If Ax = [0], the nullspace
-  // is an x that is not zero.
-  template <class MatrixT >
-  inline Matrix<typename MatrixT::value_type> nullspace( MatrixBase<MatrixT> const& A,
-                                                         typename MatrixT::value_type thresh = -1 ) {
-    typedef typename MatrixT::value_type value_type;
-    Matrix<value_type> U, V;
-    Vector<value_type> S;
-    complete_svd( A.impl(), U, S, V );
-    V = transpose(V);
-
-    size_t nty = nullity(A.impl(),U,S,V,thresh);
+    size_t nty = nullity( A.impl(), U, S, V, thresh );
     if ( nty == 0 )
-      return Matrix<value_type>(0,0);
+    {
+        return MatrixN<value_type>( 0, 0 );
+    }
     Matrix<value_type> nullsp(A.impl().cols(), nty );
     value_type th = ( thresh >= 0. ? thresh : detail::calc_threshold(A, S) );
     size_t nn = 0;
---
+++ include/terminus/math/linalg/SVD.hpp
@@ -0,0 +1,136 @@
+/**
+ * @file    SVD.hpp
+ * @author  Marvin Smith
+ * @date    11/13/2023
+*/
+#pragma once
+
+// Terminus Libraries
+#include <terminus/core/error/ErrorCategory.hpp>
+#include <terminus/math/matrix.hpp>
+
+// Boost Libraries
+#include <boost/numeric/conversion/cast.hpp>
+
+namespace tmns::math::linalg {
+
+namespace detail {
+template <typename MatrixT,
+          typename VectorT>
+typename VectorT::value_type calc_threshold( const Matrix_Base<MatrixT>& A,
+                                             const Vector_Base<VectorT>& S )
+{
+      return typename VectorT::value_type( 0.5
+                * std::sqrt( double( A.impl().cols() ) + double( A.impl().rows() ) + 1. )
+                * S.impl()[0]
+                * std::numeric_limits<typename VectorT::value_type>::epsilon() );
+}
+} // End of detail namespace
+
+/**
+ * Get the singular values
+*/
+ImageResult<VectorN<double>> svd_impl( const MatrixN<double>& A );
+
+ImageResult<void> svd_impl( const MatrixN<double>& A,
+                            MatrixN<double>&       U,
+                            VectorN<double>&       S,
+                            MatrixN<double>&       V );
+
+ImageResult<void> complete_svd_impl( const MatrixN<double>& A,
+                                     MatrixN<double>&       U,
+                                     VectorN<double>&       S,
+                                     MatrixN<double>&       V );
+
+/**
+ * Compute the singular values of a Matrix A
+ * 
+ * S will be resized if necessary to the appropriate output dimensions
+ * based on the dimensions of A.
+ */
+template <typename AMatrixT, 
+          typename SingularValuesT>
+ImageResult<SingularValuesT> svd( const AMatrixT& A )
+{
+    return svd_impl( A );
+}
+
+/**
+ * Compute the singular value decomposition of the matrix A.
+ * 
+ * U, S, and VT will be resized if necessary to the appropriate output dimensions
+ * based on the dimensions of A.
+ */
+template <typename AMatrixT,
+          typename UMatrixT,
+          typename SingularValuesT,
+          typename VTMatrixT>
+ImageResult<void> svd( const AMatrixT&  A,
+                       UMatrixT&        U,
+                       SingularValuesT& S,
+                       VTMatrixT&       VT )
+{
+    return svd_impl( A, U, S, VT );    
+}
+
+/**
+ * Compute the singular value decomposition of the matrix A, including
+ * complete orthogonal bases of the domain and range even when A is rectangular.
+ * 
+ * U, S, and VT will be resized if necessary to the appropriate output dimensions
+ * based on the dimensions of A.
+ */
+template <typename AMatrixT,
+          typename UMatrixT,
+          typename SingularValuesT,
+          typename VTMatrixT>
+ImageResult<void> complete_svd( const AMatrixT&  A,
+                                UMatrixT&        U,
+                                SingularValuesT& S,
+                                VTMatrixT&       VT )
+{
+    return complete_svd_impl( A, U, S, VT );
+}
+
+/**
+ * Solve for the rank of a matrix .. using previous SVD results
+ */
+template <typename MatrixT,
+          typename MatrixT2,
+          typename VectorT>
+int rank( const Matrix_Base<MatrixT>&   A,
+          const Matrix_Base<MatrixT2>& /*U*/,
+          const Vector_Base<VectorT>&   S,
+          const Matrix_Base<MatrixT2>& /*V*/,
+          typename VectorT::value_type thresh = -1 )
+{
+    using value_type = typename VectorT::value_type;
+    
+    value_type th = ( thresh >= 0. ? thresh : detail::calc_threshold(A, S) );
+    int nr = 0;
+    for( size_t j = 0; j < S.impl().size(); j++ )
+    {
+        if( S.impl()[j] > th )
+        {
+            nr++;
+        }
+    }
+    return nr;
+}
+
+/**
+ * Solve for the rank of a matrix.
+ */
+template <class MatrixT>
+int rank( const Matrix_Base<MatrixT>& A,
+          typename MatrixT::value_type thresh = -1 )
+{
+    using value_type = typedef typename MatrixT::value_type;
+    MatrixN<value_type> U, V;
+    VectorN<value_type> S;
+    complete_svd( A.impl(), U, S, V );
+    V = transpose(V);
+    return rank( A, U, S, V, thresh );
+}
+
+}  // End of tmns::math::linalg
